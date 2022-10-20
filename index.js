6b14d62a25e01a38245142596cf7c4c26fd0a6a3
Mime Jr <petravidovic8@gmail.com>
2022-10-19 23:29:53 -0400

    add function for writing readme

--- index.js
+++ index.js
@@ -49,10 +49,37 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (error) {
+        if (error) {
+            console.log('Could not create file.')
+        } else {
+            console.log('File successfully created.')
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer
+    .prompt(
+      questions
+    )
+    .then((answers) => {
+      console.log(answers)
+
+      let markdown = generateMarkdown(answers)
+      console.log(markdown)
+      writeToFile('README.md', markdown)
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
 
 // Function call to initialize app
 init();
