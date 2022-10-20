fc3f22e05dcd0c2ace0aca0da1b8576c143a11d4
Mime Jr <petravidovic8@gmail.com>
2022-10-19 23:32:50 -0400

    create func to generate markdown, add template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -52,6 +52,30 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing] (#contributing)
+- [Tests] (#tests)
+- [Questions] (#questions)
+
+## Installation
+
+## Usage
+
+## License
+${renderLicenseBadge(data.license)}
+
+## Contributing
+
+## Tests
+
+## Questions
+
 
 `;
 }
